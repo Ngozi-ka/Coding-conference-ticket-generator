5b6919010b761ed0649e11332feeca8421301d6b
Ngozi-ka <nwekengozika5@gmail.com>
2025-11-02 18:10:24 +0100

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const image = document.querySelector("#image-file");
 const imageLabel = document.querySelector("#image-label");
 const avatarGithub = document.querySelector(".avatar-github");
 const receivedImage = document.querySelector(".received-image");
-const firstContainer = document.querySelector(".first-container")
+const firstContainer = document.querySelector(".first-container");
 const ticketContainer = document.querySelector(".ticket-container");
 const name = document.querySelector(".namee");
 const email = document.querySelector(".emaill");
@@ -15,49 +15,43 @@ const userGit = document.getElementById("usergit");
 const todaysDate = document.querySelector(".date");
 const specialId = document.querySelector(".special-id");
 
+image.addEventListener("change", function () {
+  const file = this.files[0];
 
-image.addEventListener("change", function(){
-    const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
 
-    if (file) {
-        const reader = new FileReader();
+    reader.onload = function (e) {
+      receivedImage.src = e.target.result;
+    };
 
-        reader.onload = function(e) {
-            receivedImage.src = e.target.result;
-        }
+    reader.readAsDataURL(file);
+    imageLabel.textContent = file.name;
+  } else {
+    imageLabel.textContent = "No image selected";
+  }
+});
 
-        reader.readAsDataURL(file);
-        imageLabel.textContent = file.name;
-    }else{
-        imageLabel.textContent = "No image selected"
-    }
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  firstContainer.style.display = "none";
+  ticketContainer.style.display = "flex";
 
-form.addEventListener("submit", function(event){
-    event.preventDefault()
+  const special = Math.floor(Math.random() * 1000000);
+  const candidate = username.value;
+  const candidateName = candidate.charAt(0).toUpperCase() + candidate.slice(1);
 
-firstContainer.style.display = "none";
-ticketContainer.style.display = "flex";
+  name.textContent = candidateName;
+  yourName.textContent = candidateName;
+  email.textContent = userEmail.value;
+  yourGit.textContent = userGit.value;
+  specialId.textContent = `CONF${special}`;
 
-const special = Math.floor(Math.random() * 1000000);
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
 
-name.textContent = username.value;
-yourName.textContent = username.value;
-email.textContent = userEmail.value;
-yourGit.textContent = userGit.value
-specialId.textContent = `CONF${special}`
-
-
-
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDate();
-
-
-todaysDate.textContent = `${year}/ ${month + 1} / ${day}`
-
-})
-
-
+  todaysDate.textContent = `${year}/ ${month + 1} / ${day}`;
+});
